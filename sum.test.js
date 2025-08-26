f5b1d20c16e6ebf4fed1d6e9484be6e99b608b1b
Adithya_G <adithyaacharya710@gmail.com>
2025-08-26 19:09:05 +0530

    used describe for multiple unit testing

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,11 @@
 import {sum} from "./sum.js";
 
-test("adds 2 + 2 to equal 4", () => {
-    expect(sum(2, 2)).toBe(4);
+describe("test for sum function", () => {
+    test("adds 2 + 2 to equal 4", () => {
+        expect(sum(2, 2)).toBe(4);
+    });    
+
+    test("adds 11 + 23 to equal 34", () => {
+        expect(sum(11, 23)).toBe(34);
+    });
 });
